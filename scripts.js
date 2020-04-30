14a5bef4ae440b32a17f260176af58d81da0a883
Dori Aldasi <dorotea.juhasz@hotmail.com>
2020-04-30 17:11:05 +0200

    js változások

--- scripts.js
+++ scripts.js
@@ -1,1 +1,100 @@
 console.log("script file is loaded.");
+
+// Single line comment 
+
+/*
+  Multiple 
+  lines
+  of comments
+  */
+
+  /*
+  let name = "Dori"
+undefined
+POSThttps://www.google.com/gen_204?atyp=i&ei=-NmqXqO0F8HKrgSLva4o&ct=slh&v=t1&pv=0.5015025414114662&me=46:1588255285967,V,0,0,0,0:28129,h,1,CAwQAA,i:101,h,1,CAwQAA,o:3,h,1,CAwQAw,i:0,h,1,CAwQAA,i:180,h,1,CAwQAw,o:33,h,1,CAwQAA,o:0,h,1,CAEQAA,i:49,h,1,CAEQAA,o:1025,e,H&zx=1588255316489
+[HTTP/2 204 No Content 38ms]
+
+POSThttps://www.google.com/gen_204?atyp=i&ei=-NmqXqO0F8HKrgSLva4o&ct=slh&v=t1&pv=0.5015025414114662&me=56:1588255339522,V,0,114,1920,343:1640,e,B&zx=1588255341166
+[HTTP/2 204 No Content 37ms]
+
+POSThttps://www.google.com/gen_204?atyp=i&ei=-NmqXqO0F8HKrgSLva4o&ct=slh&v=t1&pv=0.5015025414114662&me=58:1588255341168,V,0,0,0,0:294,e,H&zx=1588255342464
+[HTTP/2 204 No Content 42ms]
+
+POSThttps://www.google.com/gen_204?atyp=i&ei=-NmqXqO0F8HKrgSLva4o&ct=slh&v=t1&pv=0.5015025414114662&me=60:1588255342833,V,0,114,1920,343:132,e,B&zx=1588255342967
+[HTTP/2 204 No Content 41ms]
+
+POSThttps://www.google.com/gen_204?atyp=i&ei=-NmqXqO0F8HKrgSLva4o&ct=slh&v=t1&pv=0.5015025414114662&me=62:1588255342970,V,0,0,0,0:4976,h,1,CAwQDQ,i:0,h,1,CAwQAA,i:183,h,1,CAwQDQ,o:1,h,1,CAwQAw,i:83,h,1,CAwQAw,o:33,h,1,CAwQAA,o:9560,V,0,114,1920,343:6214,e,B&zx=1588255364022
+[HTTP/2 204 No Content 46ms]
+
+let post = "This is my first post"
+undefined
+let post2 = "This is my second post"
+undefined
+let colors = ["orange", "blue", "green"]
+undefined
+XHRPOSThttps://play.google.com/log?format=json&hasfast=true
+[HTTP/2 200 OK 37ms]
+
+colors
+Array(3) [ "orange", "blue", "green" ]
+
+colors [0]
+"orange"
+colors [1]
+"blue"
+POSThttps://www.google.com/gen_204?atyp=i&ei=-NmqXqO0F8HKrgSLva4o&ct=slh&v=t1&pv=0.5015025414114662&me=71:1588255364025,V,0,0,0,0:644115,h,1,CAwQDQ,i:0,h,1,CAwQAA,i:32,h,1,CAwQDQ,o:1,h,1,CAwQAA,o:4,h,1,CAwQAw,i:0,h,1,CAwQAA,i:129,h,1,CAwQAw,o:33,h,1,CAwQAA,o:35,h,1,CAEQAA,i:66,h,1,CAEQAA,o:71122,e,H&zx=1588256082576
+[HTTP/2 204 No Content 136ms]
+
+POSThttps://www.google.com/gen_204?atyp=i&ei=-NmqXqO0F8HKrgSLva4o&ct=slh&v=t1&pv=0.5015025414114662&me=83:1588256083400,V,0,114,1920,343:990,h,1,CAEQAA,i:66,h,1,CAEQAA,o:32,h,1,CAwQAw,i:1,h,1,CAwQAA,i:19,h,1,CAwQAw,o:2,h,1,CAwQDQ,i:1,h,1,CAwQDQ,o:0,h,1,CAwQAA,o:1076,e,B&zx=1588256085592
+[HTTP/2 204 No Content 38ms]
+
+let pickedColor =2
+undefined
+colors [pickedColor]
+"green"
+colors.length
+3
+name
+"Dori"
+name.length
+4
+colors [3] = "lime"
+"lime"
+colors
+Array(4) [ "orange", "blue", "green", "lime" ]
+
+colors.push('purple')
+5
+colors
+Array(5) [ "orange", "blue", "green", "lime", "purple" ]
+
+POSThttps://www.google.com/gen_204?atyp=i&ei=-NmqXqO0F8HKrgSLva4o&ct=slh&v=t1&pv=0.5015025414114662&me=93:1588256085595,V,0,0,0,0:478895,h,1,CAwQDQ,i:1,h,1,CAwQAA,i:31,h,1,CAwQDQ,o:33,h,1,CAwQAA,o:1,h,1,CAEQAA,i:82,h,1,CAEQAA,o:1300,e,H&zx=1588256567939
+[HTTP/2 204 No Content 125ms]
+
+POSThttps://www.google.com/gen_204?atyp=i&ei=-NmqXqO0F8HKrgSLva4o&ct=slh&v=t1&pv=0.5015025414114662&me=101:1588256599947,V,0,114,1920,343:578,h,1,CAEQAA,i:63,h,1,CAEQAA,o:18,h,1,CAwQAA,i:33,h,1,CAwQAw,i:34,h,1,CAwQAw,o:0,h,1,CAwQDQ,i:53,h,1,CAwQDQ,o:0,h,1,CAwQAA,o:22321,e,B&zx=1588256623050
+[HTTP/2 204 No Content 42ms]
+
+colors[colors.length] ='cyan'
+"cyan"
+colors
+Array(6) [ "orange", "blue", "green", "lime", "purple", "cyan" ]
+
+let notGoodExemple = ["text", 123, true]
+undefined
+let person = {
+    name: 'Dori',
+    age: 34,
+    favoriteColors: colors,
+    oldEnoughtToDrive: true
+}
+undefined
+person
+Object { name: "Dori", age: 34, favoriteColors: (6) […], oldEnoughtToDrive: true }
+
+'Hello, dear ' + person.name
+"Hello, dear Dori"
+person.food = 'pizza'
+"pizza"
+person
+Object { name: "Dori", age: 34, favoriteColors: (6) […], oldEnoughtToDrive: true, food: "pizza" }
+*/
